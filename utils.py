396004f6e7eed20d80f10d7e923fb4db150d0b50
Vladimir Khapaev <khapaev@mail.ru>
2024-07-02 22:29:41 +0300

    update create and fill tables

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import requests
 import psycopg2
 import json
+import time
 
 
 def create_table_employers():
@@ -10,11 +11,9 @@ def create_table_employers():
         cur.execute("""
                     CREATE TABLE IF NOT EXISTS employers 
                     (
-                        employer_id int, 
-                        employer_name VARCHAR(255) NOT NULL,
-                        
-                        CONSTRAINT pk_employer_id PRIMARY KEY (employer_id)
-                    );
+                        employer_id int NOT NULL, 
+                        employer_name VARCHAR(255) NOT NULL
+                        )
                     """)
         conn.commit()
     conn.close()
@@ -57,48 +56,52 @@ def load_table_employers():
         conn.close()
 
 
-def load_table_vacancy(employer_id, url=None):
+def load_table_vacancy(url=None):
     """Получение данных с Хед Хантер и наполнение таблицы вакансиями по employer_id."""
-    url = 'https://api.hh.ru/vacancies?area=113'
-    params = {
-        'page': 5,
-        'per_page': 100
-    }
-    response = requests.get(f'{url}&employer_id={employer_id}', params)
-    data = response.json()
-    conn = psycopg2.connect(host='localhost', database='KR_5', user='postgres', password='Py091105')
-    with conn.cursor() as cur:
-        for vac in data['items']:
-            # print(vac)
-            # print()
-            vac_hh_id = vac['id']
-            epm_id = vac['employer']['id']
-            vac_name = vac.get('name')
-            vac_url = vac.get('alternate_url')
-            vac_salary = vac.get('salary')
-            if vac_salary:
-                if vac_salary.get("from") and not vac_salary.get("to"):
-                    vac_salary = vac_salary.get("from")
-                elif not vac_salary.get("from") and vac_salary.get("to"):
-                    vac_salary = vac_salary.get("to")
-                elif vac_salary.get("from") and vac_salary.get("to"):
-                    vac_salary = int((vac_salary.get("from") + vac_salary.get("to")) / 2)
-                elif not vac_salary.get("from") and not vac_salary.get("to"):
-                    vac_salary = 0
-            else:
-                vac_salary = 0
-            vac_schedule = vac['schedule']['name']
-            cur.execute(
-                'INSERT INTO vacancies (vac_hh_id, employer_id, vacancy_name, vacancy_url, salary, schedule)'
-                'VALUES (%s, %s, %s, %s, %s, %s)', (vac_hh_id, epm_id, vac_name, vac_url, vac_salary, vac_schedule))
-            conn.commit()
-        conn.close()
+    with open('data/Employers.json', 'r', encoding='utf-8') as file:
+        emp_data = json.load(file)
+        for emp, employer_id in emp_data.items():
+            time.sleep(5)
+            url = 'https://api.hh.ru/vacancies?area=113'
+            params = {
+                'page': 1,
+                'per_page': 100
+            }
+            response = requests.get(f'{url}&employer_id={employer_id}', params)
+            data = response.json()
+            conn = psycopg2.connect(host='localhost', database='KR_5', user='postgres', password='Py091105')
+            with conn.cursor() as cur:
+                for vac in data['items']:
+                    # print(vac)
+                    # print()
+                    vac_hh_id = vac['id']
+                    epm_id = vac['employer']['id']
+                    vac_name = vac.get('name')
+                    vac_url = vac.get('alternate_url')
+                    vac_salary = vac.get('salary')
+                    if vac_salary:
+                        if vac_salary.get("from") and not vac_salary.get("to"):
+                            vac_salary = vac_salary.get("from")
+                        elif not vac_salary.get("from") and vac_salary.get("to"):
+                            vac_salary = vac_salary.get("to")
+                        elif vac_salary.get("from") and vac_salary.get("to"):
+                            vac_salary = int((vac_salary.get("from") + vac_salary.get("to")) / 2)
+                        elif not vac_salary.get("from") and not vac_salary.get("to"):
+                            vac_salary = 0
+                    else:
+                        vac_salary = 0
+                    vac_schedule = vac['schedule']['name']
+                    cur.execute(
+                        'INSERT INTO vacancies (vac_hh_id, employer_id, vacancy_name, vacancy_url, salary, schedule)'
+                        'VALUES (%s, %s, %s, %s, %s, %s)', (vac_hh_id, epm_id, vac_name, vac_url, vac_salary, vac_schedule))
+                    conn.commit()
+                conn.close()
 
 
-if __name__ == "__main__":
-    create_table_employers()
-    create_table_vacancies()
-    load_table_employers()
-    load_table_vacancy('1740')
+# if __name__ == "__main__":
+#     create_table_employers()
+#     create_table_vacancies()
+#     load_table_employers()
+#     load_table_vacancy()
 
 
