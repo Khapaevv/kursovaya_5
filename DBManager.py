966b373545345d00b04f912923ed660006e9a10c
Vladimir Khapaev <khapaev@mail.ru>
2024-07-04 00:03:05 +0300

    create DBmanager functions

--- DBManager.py
+++ DBManager.py
@@ -1,5 +1,6 @@
 import psycopg2
-import self
+from tabulate import tabulate
+
 
 
 class DBManager():
@@ -17,35 +18,99 @@ class DBManager():
         conn = psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password)
         with conn.cursor() as cur:
             cur.execute("""
-                        SELECT DISTINCT employer_id FROM vacancies
+                        SELECT q.employer_name, COUNT(w.vac_hh_id)
+                        FROM employers as q
+                        JOIN vacancies as w ON q.employer_id = w.employer_id
+                        GROUP BY q.employer_name
+                        
                         """)
-            employers_list = cur.fetchall()
-            print(employers_list)
-            # for employer_name, employer_id in employers_list.items():
-            #     print(employer_id)
+            rows = cur.fetchall()
+            conn.commit()
+            conn.close()
+            headers = ["Company name", "Count of vacancies"]
+            result = tabulate(rows, headers=headers)
+            # print(result)
+            return result
+
 
     def get_all_vacancies(self):
         """получает список всех вакансий с указанием названия компании,
         названия вакансии и зарплаты и ссылки на вакансию"""
-        pass
+        conn = psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password)
+        with conn.cursor() as cur:
+            cur.execute("""
+                        SELECT q.employer_name, w.vacancy_name, w.salary, w.vacancy_url 
+                        FROM employers as q
+                        JOIN vacancies as w ON q.employer_id = w.employer_id
+
+                        """)
+            rows = cur.fetchall()
+            conn.commit()
+            conn.close()
+            headers = ["employer_name", "vacancy_name", "salary", "vacancy_url"]
+            result = tabulate(rows, headers=headers)
+            # print(result)
+            return result
 
 
-    def get_avg_salary():
+    def get_avg_salary(self):
         """получает среднюю зарплату по вакансиям"""
-        pass
+        conn = psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password)
+        with conn.cursor() as cur:
+            cur.execute("""
+                        SELECT AVG(salary)
+                        FROM vacancies
+                        WHERE salary <> 0
+                        """)
+            rows = cur.fetchall()
+            conn.commit()
+            conn.close()
+            headers = ["AVG_salary"]
+            result = tabulate(rows, headers=headers)
+            # print(result)
+            return result
 
 
-    def get_vacancies_with_higher_salary():
+    def get_vacancies_with_higher_salary(self):
         """получает список всех вакансий,
         у которых зарплата выше средней по всем вакансиям"""
-        pass
+        conn = psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password)
+        with conn.cursor() as cur:
+            cur.execute("""
+                        SELECT vacancy_name, salary
+                        FROM vacancies
+                        WHERE salary > (SELECT AVG(salary) FROM vacancies WHERE salary <> 0)
+                        """)
+            rows = cur.fetchall()
+            conn.commit()
+            conn.close()
+            headers = ["vacancy_name", "salary"]
+            result = tabulate(rows, headers=headers)
+            # print(result)
+            return result
 
 
     def get_vacancies_with_keyword(self):
         """получает список всех вакансий,
-        в названии которых содержатся переданные в метод слова, например python"""
-        pass
+        в названии которых содержатся переданные в метод слова"""
+        word = input("Введите слово")
+        conn = psycopg2.connect(host=self.host, database=self.database, user=self.user, password=self.password)
+        with conn.cursor() as cur:
+            cur.execute(f"""
+                        SELECT * FROM vacancies WHERE vacancy_name LIKE '%{word.lower()}%'
+                        """)
+            rows = cur.fetchall()
+            conn.commit()
+            conn.close()
+            headers = []
+            result = tabulate(rows, headers=headers)
+            # print(result)
+            return result
 
 if __name__ == "__main__":
     KR_5 = DBManager()
-    KR_5.get_companies_and_vacancies_count()
+    # KR_5.get_companies_and_vacancies_count()
+    # KR_5.get_all_vacancies()
+    # KR_5.get_avg_salary()
+    # KR_5.get_vacancies_with_higher_salary()
+    # KR_5.get_vacancies_with_keyword()
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import requests
 import psycopg2
 import json
 import time
+from tqdm import tqdm
 
 
 def create_table_employers():
@@ -64,39 +65,40 @@ def load_table_vacancy(url=None):
             url = 'https://api.hh.ru/vacancies?area=113'
             params = {
                 'pages': 20,
-                'page': 1,
+                'page': 0,
                 'per_page': 100
             }
-            response = requests.get(f'{url}&employer_id={employer_id}', params)
-            data = response.json()
-            print(data)
-            conn = psycopg2.connect(host='localhost', database='KR_5', user='postgres', password='Py091105')
-            with conn.cursor() as cur:
-                for vac in data['items']:
-                    # print(vac)
-                    # print()
-                    vac_hh_id = vac['id']
-                    epm_id = vac['employer']['id']
-                    vac_name = vac.get('name')
-                    vac_url = vac.get('alternate_url')
-                    vac_salary = vac.get('salary')
-                    if vac_salary:
-                        if vac_salary.get("from") and not vac_salary.get("to"):
-                            vac_salary = vac_salary.get("from")
-                        elif not vac_salary.get("from") and vac_salary.get("to"):
-                            vac_salary = vac_salary.get("to")
-                        elif vac_salary.get("from") and vac_salary.get("to"):
-                            vac_salary = int((vac_salary.get("from") + vac_salary.get("to")) / 2)
-                        elif not vac_salary.get("from") and not vac_salary.get("to"):
+            for page in tqdm(range(20), desc=f'Парсим вакансии {employer_id}', unit='вакансии', ncols=80,
+                             bar_format="{l_bar}{bar} {n_fmt}/{total_fmt}", colour="green"):
+                response = requests.get(f'{url}&employer_id={employer_id}&page={page}', params)
+                data = response.json()
+                conn = psycopg2.connect(host='localhost', database='KR_5', user='postgres', password='Py091105')
+                with conn.cursor() as cur:
+                    for vac in data['items']:
+                        # print(vac)
+                        # print()
+                        vac_hh_id = vac['id']
+                        epm_id = vac['employer']['id']
+                        vac_name = vac.get('name')
+                        vac_url = vac.get('alternate_url')
+                        vac_salary = vac.get('salary')
+                        if vac_salary:
+                            if vac_salary.get("from") and not vac_salary.get("to"):
+                                vac_salary = vac_salary.get("from")
+                            elif not vac_salary.get("from") and vac_salary.get("to"):
+                                vac_salary = vac_salary.get("to")
+                            elif vac_salary.get("from") and vac_salary.get("to"):
+                                vac_salary = int((vac_salary.get("from") + vac_salary.get("to")) / 2)
+                            elif not vac_salary.get("from") and not vac_salary.get("to"):
+                                vac_salary = 0
+                        else:
                             vac_salary = 0
-                    else:
-                        vac_salary = 0
-                    vac_schedule = vac['schedule']['name']
-                    cur.execute(
-                        'INSERT INTO vacancies (vac_hh_id, employer_id, vacancy_name, vacancy_url, salary, schedule)'
-                        'VALUES (%s, %s, %s, %s, %s, %s)', (vac_hh_id, epm_id, vac_name, vac_url, vac_salary, vac_schedule))
-                    conn.commit()
-                conn.close()
+                        vac_schedule = vac['schedule']['name']
+                        cur.execute(
+                            'INSERT INTO vacancies (vac_hh_id, employer_id, vacancy_name, vacancy_url, salary, schedule)'
+                            'VALUES (%s, %s, %s, %s, %s, %s)', (vac_hh_id, epm_id, vac_name, vac_url, vac_salary, vac_schedule))
+                        conn.commit()
+                    conn.close()
 
 
 if __name__ == "__main__":
